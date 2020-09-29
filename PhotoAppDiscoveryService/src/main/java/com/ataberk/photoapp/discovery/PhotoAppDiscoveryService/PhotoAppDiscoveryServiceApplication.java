64505fee7b49023f137f8e1eaceda11a65128b15
Ataberk Canıtez <aticanitez@hotmail.com>
2020-09-29 10:45:28 +0300

    First

---
+++ PhotoAppDiscoveryService/src/main/java/com/ataberk/photoapp/discovery/PhotoAppDiscoveryService/PhotoAppDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ataberk.photoapp.discovery.PhotoAppDiscoveryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class PhotoAppDiscoveryServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppDiscoveryServiceApplication.class, args);
+	}
+}
