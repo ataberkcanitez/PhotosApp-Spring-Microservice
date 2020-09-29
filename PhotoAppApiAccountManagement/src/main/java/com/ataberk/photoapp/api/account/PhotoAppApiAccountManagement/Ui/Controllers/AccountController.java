eccfc9b7c22a507b98277675b1ca103761a4fcff
Ataberk Canıtez <aticanitez@hotmail.com>
2020-09-29 11:37:08 +0300

    Zuul Api Gateway

--- PhotoAppApiAccountManagement/src/main/java/com/ataberk/photoapp/api/account/PhotoAppApiAccountManagement/Ui/Controllers/AccountController.java
+++ PhotoAppApiAccountManagement/src/main/java/com/ataberk/photoapp/api/account/PhotoAppApiAccountManagement/Ui/Controllers/AccountController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/account")
 public class AccountController {
 
-    @GetMapping
+    @GetMapping("/status/check")
     public String status(){
         return "Working";
     }
---
+++ PhotoAppApiZuulAPIGateway/src/test/java/com/ataberk/photoapp/api/gateway/PhotoAppApiZuulAPIGateway/PhotoAppApiZuulApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ataberk.photoapp.api.gateway.PhotoAppApiZuulAPIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiZuulApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
